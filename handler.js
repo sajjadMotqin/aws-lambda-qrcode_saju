0ee3d080feb51811e7ce26e95a768acb32ad1ced
Adel Alotaibi <adel@motqin.com>
2022-01-17 11:03:09 +0300

    fixed

--- handler.js
+++ handler.js
@@ -1,34 +1,25 @@
-'use strict';
-const QRCode = require('qrcode');
-const { base64encode } = require('nodejs-base64');
-
-let data = {
-  name: "Employee Name",
-  age: 27,
-  department: "Police",
-  id: "zAwsasas"
-};
+"use strict";
+const QRCode = require("qrcode");
+const { base64encode } = require("nodejs-base64");
 
 let stringdata = JSON.stringify(data);
 let encoded = base64encode(stringdata);
 
-module.exports.hello = async (event) => {
+module.exports.generate = (event) => {
+  return new Promise((resolve, reject) => {
+    const { name, total_amount, company, vat_number, tax_amount, date_time } =
+      event.queryStringParameters;
+    const encoded =
+      company + vat_number + date_time + total_amount + tax_amount;
     QRCode.toDataURL(encoded, function (err, code) {
-        if(err) console.log("error", err);
-        console.log(code)
-        return {
-            statusCode: 200,
-            body: JSON.stringify(
-                {
-                  message: code,
-                  input: event,
-                },
-                null,
-                2
-              ),
-          };
-       
+      if (err) console.log("error", err);
+      console.log(code);
+      resolve({
+        statusCode: 200,
+        body: JSON.stringify(encoded),
+      });
     });
+  });
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
